e84797287be4a22011a98721cc89815b5a7174a8
Casey Pye <caseypye@byui.edu>
2018-10-19 17:00:09 -0600

    submit

--- prove_05.php
+++ prove_05.php
@@ -9,13 +9,38 @@
 	<link rel="stylesheet" type="text/css" href="css/prove3.css">
     <title>DB Access</title>
   </head>
+  <?php
+	try {
+        $dbUrl = getenv('DATABASE_URL');
+                
+        $dbOpts = parse_url($dbUrl);
+                
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+                
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+        $msg = $ex->getMessage();
+        echo "Error!: $msg";
+        die();
+        }
+
+  ?>
   <body>
 	<?php include 'menu.php'; ?> <!	Add menu !>
 	<h1>Prove 5: PHP Data Access
 	<form action="cart.php" method="post">
 		<div class="container">
-			<h3>Students in database: </h3>
-			asdfasdf
+			<h3>Students in database: </h3><br>
+			<?php
+				echo $db->query("SELECT * FROM Students");
+			?>
+			
 		</div>
 	</form>
   </body>
