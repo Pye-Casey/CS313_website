4d4c3c8415abd23f5e2d761582e0ea4ad61db087
Casey Pye <caseypye@byui.edu>
2018-10-19 22:56:34 -0600

    changes

--- prove_05_info.php
+++ prove_05_info.php
@@ -81,11 +81,12 @@
 				foreach ($db->query($myQuery) as $row2)
 				{
 					$parentID = $row2['parent_id'];
-					//$parentQuery = "SELECT id, first_name, last_name, email, phone_number FROM parent WHERE id='2'";
-					//foreach ($db->query($parentQuery) as $pRow)
-					//{
-					//	echo $pRow['first_name'];
-					//}
+					$parentQuery = "SELECT id, first_name, last_name, email, phone_number FROM parent WHERE id='2'";
+					foreach ($db->query($parentQuery) as $pRow)
+					{
+						echo $pRow['first_name'];
+					}
+				}
 				?>
 					<tr>
 						<td>1</td>
