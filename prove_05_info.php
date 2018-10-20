7b83792e9b1b3beeef37d4efd5c7fa427f708300
Casey Pye <caseypye@byui.edu>
2018-10-19 22:10:46 -0600

    changes

--- prove_05_info.php
+++ prove_05_info.php
@@ -35,8 +35,9 @@
 	<?php include 'menu.php'; ?> <!	Add menu !>
 	<?php
 		//$sql = 'SELECT first_name, last_name, grade_level FROM student WHERE id="'. $_POST['id'] .'"';
-		$sql = 'SELECT first_name, last_name, grade_level FROM student WHERE id=2';
+		$sql = "SELECT first_name, last_name, grade_level FROM student WHERE id = '2'";
 		$statement = $db->query($sql);
+		$results = $statement->fetchAll(PDO::FETCH_ASSOC);
 		echo $_POST['id'];
 		echo $statement['first_name'];
 		//echo $statement['last_name'];
