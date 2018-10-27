bd32e138ef5983c128089ebd8506cf303202d938
Casey Pye <caseypye@byui.edu>
2018-10-26 20:24:11 -0600

    add code

--- prove_06_student.php
+++ prove_06_student.php
@@ -31,40 +31,13 @@
     }
 	
 	function addParent($fName,$lName, $grade) {
-			try {
-			$dbUrl = getenv('DATABASE_URL');
-					
-			$dbOpts = parse_url($dbUrl);
-					
-			$dbHost = $dbOpts["host"];
-			$dbPort = $dbOpts["port"];
-			$dbUser = $dbOpts["user"];
-			$dbPassword = $dbOpts["pass"];
-			$dbName = ltrim($dbOpts["path"],'/');
-					
-			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-					
-			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			
-			//Customized
-			$dbInstance = db::getInstance();
-			$insertStatement = $dbInstance->prepare("INSERT INTO student(first_name, last_name, grade_level) VALUES (?,?,?)");
-			$insertStatement->bind_param($fName,$lName,$grade);
-			// clean up just in case
+		// clean up just in case
 			$fName =  htmlspecialchars($fName);
 			$lName =  htmlspecialchars($lName);
 			$grade =  htmlspecialchars($grade);
-			$insertStatement->execute();
-			
-		} catch (PDOException $ex) {
-			$msg = $ex->getMessage();
-			echo "Error!: $msg";
-			die();
-		}
-			
-			// add to database
+		// add to database
 			//$query = 
-			//$insertStatement = $db->query("INSERT INTO student(first_name, last_name, grade_level) VALUES ('Pacey', 'Cye', 7)");
+		$insertStatement = $db->query("INSERT INTO student(first_name, last_name, grade_level) VALUES ('"$fName"', '"$lName"', "$grade")");
 			// Use prepared statements
 			/*$insertStatement = $db->prepare("INSERT INTO student(first_name, last_name, grade_level) VALUES (?,?,?)");
 			$insertStatement->bind_param($fName,$lName,$grade);
@@ -100,6 +73,7 @@
 		
 		//test insert
 		//$db->query("INSERT INTO student('first_name', 'last_name', 'grade_level') VALUES ('Pacey', 'Cye', 7)");
+		//WORKS
 		//$insertStatement = $db->query("INSERT INTO student(first_name, last_name, grade_level) VALUES ('Pacey', 'Cye', 7)");
 		
 	?>
