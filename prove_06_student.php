2638b0bda09b8b3e1e7204c00064725518f1e6a1
Casey Pye <caseypye@byui.edu>
2018-10-26 18:21:32 -0600

    add code

--- prove_06_student.php
+++ prove_06_student.php
@@ -70,6 +70,8 @@
 		//addParent($fName, $lName, $grade);
 		//$db->query("INSERT INTO student('first_name', 'last_name', 'grade_level') VALUES ('Pacey', 'Cye', 7)");
 		$stmt = $db->query('SELECT first_name FROM student WHERE id=1');
+		echo $stmt;
+		
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 		echo $row;
 		
