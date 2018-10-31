cf41403ee611cb62203c29fa4a3cfd6b72511f0b
Casey Pye <caseypye@byui.edu>
2018-10-30 20:15:48 -0600

    test

--- week7/doLogin.php
+++ week7/doLogin.php
@@ -38,6 +38,15 @@
 			$stmt->execute();
 			// get rows
 			$row = $stmt->fetch(PDO::FETCH_ASSOC);
+			$hash = $row["password"];
+			// See the password_hash() example to see where this came from.
+			//$hash = '$2y$07$BCryptRequires22Chrcte/VlQH0piJtjXl.0t1XkA8pw9dMXTpOq';
+
+			if (password_verify($password, $hash)) {
+			echo 'Password is valid!';
+			} else {
+			echo 'Invalid password.';
+			}
 			var_dump($row);
 			die();
 			
