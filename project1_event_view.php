250d274d28acc65f7060be162d21205057c35353
Casey Pye <caseypye@byui.edu>
2018-11-02 15:41:24 -0600

    view button

--- project1_event_view.php
+++ project1_event_view.php
@@ -1,3 +1,44 @@
+<?php
+	try {
+        $dbUrl = getenv('DATABASE_URL');
+                
+        $dbOpts = parse_url($dbUrl);
+                
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+                
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = 'SELECT * FROM behavior.events AS events, behavior.students AS students WHERE events.student_id = students.id';
+		
+		foreach ($db->query(sql) as $row)
+			{
+				if ($row['id'] == $_POST['id']) {
+					$id = $row['id'];
+					$fName = $row['first_name'];
+					$lName = $row['last_name'];
+					$time = $row['time'];
+					$location = $row['location'];
+					$grade = $row['grade_level'];
+					$description = $row['description'];
+					$date = $row['date'];
+					$staff = $row['staff_name'];
+					//echo $row['first_name'];
+				}
+				
+			}
+		
+        } catch (PDOException $ex) {
+			$msg = $ex->getMessage();
+			echo "Error!: $msg";
+			die();
+        }
+
+  ?>
 <!DOCTYPE html>
 <html>
   
@@ -18,25 +59,24 @@
 	<h2><label name="info">Event View </label></h2>
 	<p>The following are the details to the requested behavior request.</p>
 	<div class="form-group">
-		<label for="studentName">Student Name:<?php echo $_POST["first_Name"] . " " . $_POST["last_name"] ?></label>
-		<label type="text" class="" id="studentName"></label>
+		<label for="studentName">Student Name:</label>
+		<label type="text" class="" id="studentName"><?php echo $fName . " " . $lName ;?></label>
 	</div>
-	
 	<div class="form-group">
 		<label for="grade">Grade Level:</label>
-		<label type="number" name="gradeLevel" id="grade"></label>
+		<label type="text" class="" id="grade"><?php echo $grade ;?></label>
 	</div>
 	<div class="form-group">
 		<label for="staffName">Reffering Staff:</label>
-		<label type="text" class="" id="staffName"></label>
+		<label type="text" class="" id="staffName"><?php echo $staff ;?></label>
 	</div>
 	<div class="form-group">
 		<label for="location">Location:</label>
-		<label type="text" class="" id="location"></label>
+		<label type="text" class="" id="location"><?php echo $location ;?></label>
 	</div>
 	<div class="form-group">
 		<label for="description">Description:</label>
-		<label type="text" class="" id="description"></label>
+		<label type="text" class="" id="description"><?php echo $description ;?></label>
 	</div>
 	
 	<button type="submit"  class="btn btn-success" >Back</button>
