fc7e2652492a4dfb807935219e86da4dff484a3e
Casey Pye <caseypye@byui.edu>
2018-11-02 20:48:24 -0600

    view button

--- project1_student_add.php
+++ project1_student_add.php
@@ -1,3 +1,25 @@
+<?php
+	try {
+        $dbUrl = getenv('DATABASE_URL');
+                
+        $dbOpts = parse_url($dbUrl);
+                
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+                
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+        $msg = $ex->getMessage();
+        echo "Error!: $msg";
+        die();
+        }
+
+  ?>
 <!DOCTYPE html>
 <html>
   
@@ -19,15 +41,15 @@
 	<h2>Add a Student</h2>
 	<div class="form-group">
 		<label for="fName">First Name:</label>
-		<input type="text" class="form-control" id="fName">
+		<input type="text" class="form-control" id="fName" name="fName">
 	</div>
 	<div class="form-group">
 		<label for="lName">Last Name:</label>
-		<input type="text" class="form-control" id="lName">
+		<input type="text" class="form-control" id="lName" name="lName">
 	</div>
 	<div class="form-group">
 		<label for="grade">Grade Level:</label>
-		<input type="number" name="gradeLevel" min="0" step="1" class="form-control" id="grade">
+		<input type="number" name="gradeLevel" min="0" step="1" name="grade" class="form-control" id="grade">
 	</div>
 	<button type="submit" class="btn btn-primary" >Create Student</button>
 	</form>
--- project1_student_added.php
+++ project1_student_added.php
@@ -15,6 +15,46 @@
 	<?php include 'project_menu.php'; ?> <!	Add menu !>
 	<div class="container">
 	<form action="project1_student_add.php" >
+	<?php
+		// add event
+		try {
+        $dbUrl = getenv('DATABASE_URL');
+                
+        $dbOpts = parse_url($dbUrl);
+                
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+                
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		// clean up just in case
+			$fName =  htmlspecialchars($_POST["fName"]);
+			$lName =  htmlspecialchars($_POST["lName"]);
+			$grade =  htmlspecialchars($_POST["grade"]);
+			$description =  htmlspecialchars($_POST["description"]);
+			
+		// add to database
+		$query = "INSERT INTO behavior.students (first_name, last_name, grade_level) VALUES(:fName, :lName, :grade)";
+		// create this new row
+		$stmt_insert_user = $db->prepare($query);
+		$stmt_insert_user->bindValue(':fName', $fName);
+		$stmt_insert_user->bindValue(':lName', $lName);
+		$stmt_insert_user->bindValue(':grade', $grade);
+		
+		$stmt_insert_user->execute(); 
+		echo "<h2><label name='info'>Student Added!</label></h2>";
+		} catch (PDOException $ex) {
+			$msg = $ex->getMessage();
+			echo "<h2>Error: Student was not added. Please try again later.</h2>";
+			die();
+		}	
+	?>
+	
 	<h2><label name="info">Student Added!</label></h2>
 	<p>The following student has been added.</p>
 	<div class="form-group">
